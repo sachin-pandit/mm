d0e6d6992c40bf3fcdfc0aa0940b3d9aa3a35482
Sachin Pandit <sachinspandits@gmail.com>
2016-09-06 22:40:39 -0700

    Original Stage - As given my prof

--- Main.cpp
+++ Main.cpp
@@ -206,7 +206,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				hdc = BeginPaint(hWnd, &ps);
 				// TO DO: Add any drawing code here...
 				char text[1000];
-				strcpy(text, "Original image (Left)  Image after modification (Right1)\n");
+				strcpy(text, "Original image (Left)  Image after modification (Right)\n");
 				DrawText(hdc, text, strlen(text), &rt, DT_LEFT);
 				strcpy(text, "\nUpdate program with your code to modify input image");
 				DrawText(hdc, text, strlen(text), &rt, DT_LEFT);
